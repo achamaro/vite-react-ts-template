92a3f4e063eef0e13e94b0ed8153507eda422fcc
achamaro <a.achamaro@gmail.com>
2023-05-04 12:13:27 +0900

    chore: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import icon from "@achamaro/vite-plugin-icon";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [icon(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
